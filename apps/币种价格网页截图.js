949956fc1e4633abf6172586d1e6d93276242071
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-08-17 14:41:15 +0000

    add apps/币种价格网页截图.js.
    Signed-off-by: 叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>

---
+++ apps/币种价格网页截图.js
@@ -0,0 +1,77 @@
+import plugin from '../../../lib/plugins/plugin.js';
+import { segment } from 'oicq';
+import puppeteer from 'puppeteer';
+
+export class WebPreview extends plugin {
+
+  constructor() {
+    super({
+      name: '网页预览',
+      dsc: '发送#币种,返回此币种价格网页截图',
+
+      event: 'message',
+      priority: 100,
+
+      rule: [
+        {
+          reg: '^#?dnx$',
+          fnc: 'preview'
+        },
+        {
+          reg: '^#?kas$',
+          fnc: 'previewKaspa'
+        },
+        {
+          reg: '^#?rvn$',
+          fnc: 'previewRavencoin'
+        }
+      ]
+    });
+  }
+
+  async preview() {
+    const url = 'https://www.coincarp.com/zh/currencies/dynex/';
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.setViewport({ width: 1000, height: 800 });
+
+    const imgBuffer = await page.screenshot();
+
+    await browser.close();
+
+    await this.reply(segment.image(imgBuffer));
+  }
+
+  async previewKaspa() {
+    const url = 'https://www.coincarp.com/zh/currencies/kaspa/';
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.setViewport({ width: 1000, height: 800 });
+
+    const imgBuffer = await page.screenshot();
+
+    await browser.close();
+
+    await this.reply(segment.image(imgBuffer));
+  }
+
+  async previewRavencoin() {
+    const url = 'https://www.coincarp.com/zh/currencies/ravencoin/';
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.setViewport({ width: 1000, height: 800 });
+
+    const imgBuffer = await page.screenshot();
+
+    await browser.close();
+
+    await this.reply(segment.image(imgBuffer));
+  }
+
+}
