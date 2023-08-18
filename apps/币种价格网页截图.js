42383647a7524e18e41edd8677e39b14e6558464
一只可爱的MEMZ <2954439244@qq.com>
2023-08-18 14:17:54 +0000

    update apps/币种价格网页截图.js.
    Signed-off-by: 一只可爱的MEMZ <2954439244@qq.com>

--- apps/币种价格网页截图.js
+++ apps/币种价格网页截图.js
@@ -3,14 +3,15 @@ import { segment } from 'oicq';
 import puppeteer from 'puppeteer';
 
 export class WebPreview extends plugin {
-  browser = null;
 
   constructor() {
     super({
       name: '网页预览',
       dsc: '发送#币种,返回此币种价格网页截图',
+
       event: 'message',
       priority: 100,
+
       rule: [
         {
           reg: '^#?dnx$',
@@ -34,66 +35,99 @@ export class WebPreview extends plugin {
         },
         {
           reg: '^#?clore$',
-          fnc: 'previewClore'
+          fnc: 'previewclore'
         }
       ]
     });
   }
 
-  async initBrowser() {
-    if (!this.browser) {
-      this.browser = await puppeteer.launch();
-    }
-  }
+  async preview() {
+    const url = 'https://www.coincarp.com/zh/currencies/dynex/';
 
-  async closeBrowser() {
-    if (this.browser) {
-      await this.browser.close();
-      this.browser = null;
-    }
-  }
-
-  async preview(url) {
-    await this.initBrowser();
-    const page = await this.browser.newPage();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
     await page.goto(url);
-    await page.setViewport({ width: 1000, height: 600 });
+    await page.setViewport({ width: 1000, height: 800 });
+
     const imgBuffer = await page.screenshot();
-    await page.close();
+
+    await browser.close();
+
+    await this.reply(segment.image(imgBuffer));
+  }
+
+  async previewKaspa() {
+    const url = 'https://www.coincarp.com/zh/currencies/kaspa/';
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.setViewport({ width: 1000, height: 800 });
+
+    const imgBuffer = await page.screenshot();
+
+    await browser.close();
+
+    await this.reply(segment.image(imgBuffer));
+  }
+
+  async previewRavencoin() {
+    const url = 'https://www.coincarp.com/zh/currencies/ravencoin/';
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.setViewport({ width: 1000, height: 800 });
+
+    const imgBuffer = await page.screenshot();
+
+    await browser.close();
+
     await this.reply(segment.image(imgBuffer));
   }
 
   async previewBitcoin() {
     const url = 'https://www.coincarp.com/zh/currencies/bitcoin/';
-    await this.preview(url);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.setViewport({ width: 1000, height: 800 });
+
+    const imgBuffer = await page.screenshot();
+
+    await browser.close();
+
+    await this.reply(segment.image(imgBuffer));
   }
 
   async previewChia() {
     const url = 'https://www.coincarp.com/zh/currencies/chianetwork/';
-    await this.preview(url);
-  }
 
-  async previewClore() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.setViewport({ width: 1000, height: 800 });
+
+    const imgBuffer = await page.screenshot();
+
+    await browser.close();
+
+    await this.reply(segment.image(imgBuffer));
+  }
+  async previewclore() {
     const url = 'https://www.coincarp.com/zh/currencies/clore-ai/price/';
-    await this.preview(url);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.setViewport({ width: 1000, height: 800 });
+
+    const imgBuffer = await page.screenshot();
+
+    await browser.close();
+
+    await this.reply(segment.image(imgBuffer));
   }
 
-  async previewKaspa() {
-    const url = 'https://www.coincarp.com/zh/currencies/kaspa/';
-    await this.preview(url);
-  }
-
-  async previewRavencoin() {
-    const url = 'https://www.coincarp.com/zh/currencies/ravencoin/';
-    await this.preview(url);
-  }
-
-  async preview() {
-    const url = 'https://www.coincarp.com/zh/currencies/dynex/';
-    await this.preview(url);
-  }
-
-  async onDisable() {
-    await this.closeBrowser();
-  }
 }
