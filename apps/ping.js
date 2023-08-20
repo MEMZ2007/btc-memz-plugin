e12bdcf9ebae3006816ce28113fbd5c7cb799933
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-08-20 05:40:38 +0000

    update apps/ping.js.
    Signed-off-by: 叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>

--- apps/ping.js
+++ apps/ping.js
@@ -11,7 +11,7 @@ export class Ping extends plugin {
       name: 'Ping',
       dsc: 'Ping',
       event: 'message',
-      priority: 6,
+      priority: -1,
       rule: [
         {
           reg: '^#?[pP]ing ',
