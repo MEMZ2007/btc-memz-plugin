a5ea4de8264559e7125a7ed553f70dc3bb9fcfad
枫林 <11918480+lin-zhi-xuan@user.noreply.gitee.com>
2023-08-26 14:41:26 +0000

    优化
    Signed-off-by: 枫林 <11918480+lin-zhi-xuan@user.noreply.gitee.com>

--- apps/bizhong.js
+++ apps/bizhong.js
@@ -1,85 +1,45 @@
-import puppeteer from 'puppeteer';
-import { segment } from 'oicq';
 import plugin from '../../../lib/plugins/plugin.js';
+import { segment } from 'oicq';
+import puppeteer from 'puppeteer';
+
+const url_list = {
+  'dnx': "https://www.coincarp.com/zh/currencies/dynex/",
+  'kas': "https://www.coincarp.com/zh/currencies/kaspa/",
+  'rvn': "https://www.coincarp.com/zh/currencies/ravencoin/",
+  'btc': "https://www.coincarp.com/zh/currencies/bitcoin/",
+  'chia': "https://www.coincarp.com/zh/currencies/chianetwork/",
+  'clore': "https://www.coincarp.com/zh/currencies/clore-ai/price/",
+  'doge': "https://www.coincarp.com/zh/currencies/dogecoin/",
+  'ergo': "https://www.bibiqing.com/coin/ergoplatform",
+  'eth': "https://www.coincarp.com/zh/currencies/ethereum/",
+  'nexa': "https://www.coincarp.com/zh/currencies/nexa-org/",
+  'neoxa': "https://www.mytokencap.com/zh/currencies/neox/821838646/",
+  'rxd': "https://www.coincarp.com/zh/currencies/radiant/",
+  'xch': "https://www.coincarp.com/zh/currencies/chianetwork/",
+  'meme': "https://www.coincarp.com/zh/currencies/meme-bsc/",
+  'pepe': "https://www.coincarp.com/zh/currencies/pepe/",
+};
 
 export class WebPreview extends plugin {
   constructor() {
     super({
       name: '网页预览',
-      dsc: '发送#币种,返回此币种价格网页截图',
+      dsc: '发送#查询币种,返回此币种价格网页截图',
       event: 'message',
       priority: 100,
       rule: [
         {
-          reg: '^#?dnx$',
+          reg: `^#(dnx|kas|rvn|btc|chia|clore|doge|ergo|eth|nexa|neoxa|rxd|xch|meme|pepe)$`,
           fnc: 'preview'
         },
-        {
-          reg: '^#?kas$',
-          fnc: 'previewKaspa'
-        },
-        {
-          reg: '^#?rvn$',
-          fnc: 'previewRavencoin'
-        },
-        {
-          reg: '^#?btc$',
-          fnc: 'previewBitcoin'
-        },
-        {
-          reg: '^#?chia$',
-          fnc: 'previewChia'
-        },
-        {
-          reg: '^#?clore$',
-          fnc: 'previewclore'
-        },
-        {
-          reg: '^#?doge$',
-          fnc: 'previewdoge'
-        },
-        {
-          reg: '^#?ergo$',
-          fnc: 'previewergo'
-        },
-        {
-          reg: '^#?eth$',
-          fnc: 'previeweth'
-        },
-        {
-          reg: '^#?nexa$',
-          fnc: 'previewnexa'
-        },
-        {
-          reg: '^#?rvn$',
-          fnc: 'previewrvn'
-        },
-        {
-          reg: '^#?neoxa$',
-          fnc: 'neoxa'
-        },
-        {
-          reg: '^#?rxd$',
-          fnc: 'rxd'
-        },
-        {
-          reg: '^#?xch$',
-          fnc: 'xch'
-        },
-        {
-          reg: '^#?meme$',
-          fnc: 'meme'
-        },
-        {
-          reg: '^#?pepe$',
-          fnc: 'pepe'
-        }
-      ],
+      ]
     });
   }
 
-  async takeScreenshot(url) {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+  async preview(e) {
+    let name = e.msg.replace(/#/g,'').trim();
+    const url = url_list[name];
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
     await page.setViewport({ width: 1000, height: 800 });
@@ -87,58 +47,4 @@ export class WebPreview extends plugin {
     await browser.close();
     await this.reply(segment.image(imgBuffer));
   }
-
-  async preview() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/dynex/');
-  }
-  async previewKaspa() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/kaspa/');
-  }
-  async previewRavencoin() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/ravencoin/');
-  }
-  async previewBitcoin() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/bitcoin/');
-  }
-  async previewChia() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/chianetwork/');
-  }
-  async previewclore() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/clore-ai/price/');
-  }
-  async previewdoge() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/dogecoin/');
-  }
-  async previewergo() {
-    await this.takeScreenshot('https://www.bibiqing.com/coin/ergoplatform');
-  }
-  async previeweth() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/ethereum/');
-  }
-  async previewnexa() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/nexa-org/');
-  }
-  async previewrvn() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/ravencoin/');
-  }
-  async neoxa() {
-    await this.takeScreenshot('https://www.mytokencap.com/zh/currencies/neox/821838646/');
-  }
-  async rxd() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/radiant/');
-  }
-  async xch() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/chianetwork/');
-  }
-  async meme() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/meme-bsc/');
-  }
-  async pepe() {
-    await this.takeScreenshot('https://www.coincarp.com/zh/currencies/pepe/');
-  }
-  //QAQ，好累啊
-  //MD，累死我了
-  //AWA
-  //这优化
-  //欸嘿
 }
