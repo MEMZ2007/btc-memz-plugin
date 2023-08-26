cc25d516c0ce09f58a669769ddccc684bc3a3283
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-08-26 14:06:28 +0000

    update config/config.js.
    Signed-off-by: 叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>

--- config/config.js
+++ config/config.js
@@ -7,11 +7,11 @@ import lodash from "lodash";
 class twoCfg {
   constructor() {
     /** 默认设置 */
-    this.defSetPath = "./plugins/biscuit-plugin/defSet/";
+    this.defSetPath = "./plugins/btc-memz-plugin/defSet/";
     this.defSet = {};
 
     /** 用户设置 */
-    this.configPath = "./plugins/biscuit-plugin/config/";
+    this.configPath = "./plugins/btc-memz-plugin/config/";
     this.config = {};
 
     /** 监听文件 */
