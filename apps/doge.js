5bb1df3a36835d26a49768627deee1da28f9b49c
一只可爱的MEMZ <2954439244@qq.com>
2023-08-26 06:29:09 +0000

    增加屎山（
    Signed-off-by: 一只可爱的MEMZ <2954439244@qq.com>

---
+++ apps/doge.js
@@ -0,0 +1,40 @@
+import plugin from '../../lib/plugins/plugin.js';
+ import { segment } from 'oicq';
+ import puppeteer from 'puppeteer';
+ 
+ export class WebPreview extends plugin {
+ 
+   constructor() {
+     super({
+       name: '网页预览',
+       dsc: '发送#doge,返回kas价格网页截图',
+       
+       event: 'message',
+       priority: 100,
+ 
+       rule: [
+         {
+           reg: '^doge$', 
+           fnc: 'preview' 
+         }
+       ]
+     });
+   }
+ 
+   async preview() {
+ 
+     const url = 'https://www.coincarp.com/zh/currencies/dogecoin/';
+     
+     const browser = await puppeteer.launch();
+     const page = await browser.newPage();
+     await page.goto(url);
+     await page.setViewport({ width: 1000, height: 800 });
+     
+     const imgBuffer = await page.screenshot(); 
+ 
+     await browser.close();
+ 
+     await this.reply(segment.image(imgBuffer));
+   }
+ 
+ }
