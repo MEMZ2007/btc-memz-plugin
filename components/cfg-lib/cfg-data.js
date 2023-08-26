6ed57658a3d83ae606875a5cfc45a1b1c46c145f
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-08-26 14:03:51 +0000

    update components/cfg-lib/cfg-data.js.
    Signed-off-by: 叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>

--- components/cfg-lib/cfg-data.js
+++ components/cfg-lib/cfg-data.js
@@ -6,7 +6,7 @@ import fs from 'node:fs'
 let cfgData = {
   async loadOldData () {
     const _path = process.cwd()
-    const _cfgPath = `${_path}/plugins/biscuit-plugin/components/`
+    const _cfgPath = `${_path}/plugins/btc-memz-plugin/components/`
     if (!fs.existsSync(_cfgPath + 'cfg.json')) {
       return false
     }
