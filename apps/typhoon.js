3ac2648071d9ca835c1e85e4862a78d5ef300da8
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-08-27 02:07:57 +0000

    update apps/typhoon.js.
    Signed-off-by: 叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>

--- apps/typhoon.js
+++ apps/typhoon.js
@@ -1,33 +1,45 @@
 import plugin from '../../../lib/plugins/plugin.js';
 import puppeteer from 'puppeteer';
 
-export class TyphoonInfo extends plugin {
+export class TyphoonScreenshot extends plugin {
   constructor() {
     super({
-      name: '台风信息查询',
+      name: '台风截图',
       dsc: '发送#台风，获取最新台风信息截图',
       event: 'message',
       priority: 100,
       rule: [
         {
-          reg: '^#?台风$',
-          fnc: 'getTyphoonInfo',
+          reg: '^#台风$',
+          fnc: 'captureTyphoonScreenshot',
         },
       ],
     });
   }
 
-  async getTyphoonInfo(e) {
+  async captureTyphoonScreenshot(e) {
     try {
       const browser = await puppeteer.launch();
+      const page = await browser.newPage();
       const url = 'http://typhoon.nmc.cn/web.html';
+
+      // 打开指定网页
       await page.goto(url);
+
+      // 等待 3 秒钟
+      await page.waitForTimeout(3000);
+
+      // 截取网页截图
       const imgBuffer = await page.screenshot();
+
+      // 关闭浏览器
       await browser.close();
+
+      // 发送截图
       await this.reply(segment.image(imgBuffer));
     } catch (error) {
-      console.error('获取台风信息失败:', error);
-      await this.reply('获取台风信息失败，请稍后重试。');
+      console.error('获取台风截图失败:', error);
+      await this.reply('获取台风截图失败，请稍后重试。');
     }
   }
 }
