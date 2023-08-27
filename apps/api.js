fedd4eebef7fd3d0bbdaeecb3ae557a6e69a0199
一只可爱的MEMZ <2954439244@qq.com>
2023-08-27 10:38:20 +0000

    发送屌图（
    Signed-off-by: 一只可爱的MEMZ <2954439244@qq.com>

---
+++ apps/api.js
@@ -0,0 +1,29 @@
+import plugin from '../../lib/plugins/plugin.js';
+import { segment } from 'oicq';
+import fetch from 'node-fetch';
+
+export class WebPreview extends plugin {
+
+  constructor() {
+    super({
+      name: 'api叼图发送',
+      dsc: '发送#api,返回api发送图片',
+      event: 'message',
+      priority: 100,
+      rule: [
+        {
+          reg: '^#?api$', 
+          fnc: 'preview' 
+        }
+      ]
+    });
+  }
+
+  async preview() {
+    const url = 'http://192.168.1.11:8000/image';
+    const response = await fetch(url);
+    const buffer = await response.buffer();
+    await this.reply(segment.image(buffer));
+  }
+
+}
