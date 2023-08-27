eb77aaefa6e428cf76de06379a640d5be27c1c37
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-08-27 01:49:18 +0000

    add apps/awa.js.
    Signed-off-by: 叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>

---
+++ apps/awa.js
@@ -0,0 +1,45 @@
+import plugin from '../../../lib/plugins/plugin.js';
+import puppeteer from 'puppeteer';
+
+export class TyphoonInfo extends plugin {
+  constructor() {
+    super({
+      name: '台风信息查询',
+      dsc: '发送#台风，获取最新台风信息截图',
+      event: 'message',
+      priority: 100,
+      rule: [
+        {
+          reg: '^#?台风$',
+          fnc: 'getTyphoonInfo',
+        },
+      ],
+    });
+  }
+
+  async getTyphoonInfo(e) {
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      const url = 'http://typhoon.nmc.cn/web.html';
+
+      // 打开台风信息网页
+      await page.goto(url);
+
+      // 等待 10 秒钟
+      await page.waitForTimeout(10000);
+
+      // 截取网页截图
+      const imgBuffer = await page.screenshot();
+
+      // 关闭浏览器
+      await browser.close();
+
+      // 发送截图
+      await this.reply(segment.image(imgBuffer));
+    } catch (error) {
+      console.error('获取台风信息失败:', error);
+      await this.reply('获取台风信息失败，请稍后重试。');
+    }
+  }
+}
