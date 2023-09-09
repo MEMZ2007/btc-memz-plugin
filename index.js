ccdc9af9ab7d48347c966ef8772f5cb81930daaf
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-09-09 15:34:02 +0000

    七岁那年和十七岁那年，中间有十年，十七岁那年和二十七岁这年，中间有一生。
    Signed-off-by: 叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
 import fs from 'node:fs'
 import chalk from 'chalk'
 
-// 定义要加载的文件夹路径列表
-const foldersToLoad = ['./plugins/btc-memz-plugin/apps', './plugins/btc-memz-plugin/apps/cmd', './plugins/btc-memz-plugin/apps/api']
+const files = fs.readdirSync('./plugins/btc-memz-plugin/apps').filter(file => file.endsWith('.js'))
 
-// 用于存储所有加载的插件
-let apps = {}
-
-for (const folder of foldersToLoad) {
-  // 获取文件夹中以.js结尾的文件列表
-  const files = fs.readdirSync(folder).filter(file => file.endsWith('.js'))
-
-  files.forEach((file) => {
-    // 构建完整的文件路径
-    const filePath = `${folder}/${file}`
-    const name = file.replace('.js', '')
-
-    try {
-      // 动态导入插件并添加到apps对象中
-      const plugin = await import(filePath)
-      apps[name] = plugin
-    } catch (error) {
-      logger.error(`载入插件错误：${name}`)
-      logger.error(error)
-    }
-  })
-}
-
+let ret = []
+ 
 logger.info(`\n\t${chalk.white(`┌───────────────────────────┐`)}\t\n\t${chalk.cyan(`「btc-memz-plugin载入中···」`)}\n\t${chalk.blue(`「载入成功！」`)}\n\t${chalk.yellow(`「交流群号：235589956   」`)}\n\t${chalk.white(`└───────────────────────────┘`)}\t`);
+files.forEach((file) => {
+  ret.push(import(`./apps/${file}`))
+})
 
+ret = await Promise.allSettled(ret)
+
+let apps = {}
+for (let i in files) {
+  let name = files[i].replace('.js', '')
+
+  if (ret[i].status != 'fulfilled') {
+    logger.error(`载入插件错误：${logger.red(name)}`)
+    logger.error(ret[i].reason)
+    continue
+  }
+  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+}
 export { apps }
