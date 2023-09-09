566eb3c173cf75f1be61d7c35aa5f76c8df0da84
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-09-09 15:29:59 +0000

    我从未放弃喜欢你，只是从浓烈变得悄无声息。
    Signed-off-by: 叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 import fs from 'node:fs'
 import chalk from 'chalk'
 
+// 定义要加载的文件夹路径列表
 const foldersToLoad = ['./plugins/btc-memz-plugin/apps', './plugins/btc-memz-plugin/apps/cmd', './plugins/btc-memz-plugin/apps/api']
 
+// 用于存储所有加载的插件
 let apps = {}
 
 for (const folder of foldersToLoad) {
+  // 获取文件夹中以.js结尾的文件列表
   const files = fs.readdirSync(folder).filter(file => file.endsWith('.js'))
 
   files.forEach((file) => {
+    // 构建完整的文件路径
     const filePath = `${folder}/${file}`
     const name = file.replace('.js', '')
 
     try {
+      // 动态导入插件并添加到apps对象中
       const plugin = await import(filePath)
       apps[name] = plugin
     } catch (error) {
@@ -22,14 +27,6 @@ for (const folder of foldersToLoad) {
   })
 }
 
-logger.info(`\n\t${chalk.white(`┌───────────────────────────┐`)}\t\n\t${chalk.cyan(`「btc-memz-plugin载入中···」`)}\n\t${chalk.blue(`「载入成功！」`)}\n\t${chalk.yellow(`「交流群号：235589956   」`)}\n\t${chalk.white(`└───────────────────────────┘`)}
-\t\n\t${chalk.cyan(`原谅我孤陋寡闻，不知先生心里一直有人。`)}\t\n\t
-\t\n\t${chalk.cyan(`因为喜欢你所以想借着你的光去看看从未见过的世界。`)}\t\n\t
-\t\n\t${chalk.cyan(`我好像一直在放弃他，又好像一直在等他。`)}\t\n\t
-\t\n\t${chalk.cyan(`所有不合时宜的相遇啊，都遗憾得令人心疼。`)}\t\n\t
-\t\n\t${chalk.cyan(`我怕他知道，怕他不知道，怕他装作不知道。`)}\t\n\t
-\t\n\t${chalk.cyan(`我没有在等他，我在等心死。`)}\t\n\t
-\t\n\t${chalk.cyan(`难过怎么说得清楚，失落又怎能感同身受。`)}
-\t`);
+logger.info(`\n\t${chalk.white(`┌───────────────────────────┐`)}\t\n\t${chalk.cyan(`「btc-memz-plugin载入中···」`)}\n\t${chalk.blue(`「载入成功！」`)}\n\t${chalk.yellow(`「交流群号：235589956   」`)}\n\t${chalk.white(`└───────────────────────────┘`)}\t`);
 
 export { apps }
