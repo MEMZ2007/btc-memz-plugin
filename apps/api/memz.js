d62360789a6608841ccd5afef83ab2c4b6934890
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-09-09 04:14:31 +0000

    我想去拥抱你，即便山河阻拦，风雨交加，路遥马远，我都可以跨越重重艰险去找到你，你知我从未惧怕奔赴，唯独你不爱我，会使我连迈起脚尖的勇气都没有。
    Signed-off-by: 叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>

---
+++ apps/api/memz.js
@@ -0,0 +1,60 @@
+import plugin from '../../../../lib/plugins/plugin.js';
+import { segment } from 'oicq';
+import fetch from 'node-fetch';
+
+export class WebPreview extends plugin {
+
+  constructor() {
+    super({
+      name: 'api叼图发送',
+      dsc: '发送#随机图片,返回api发送图片',
+      event: 'message',
+      priority: 100,
+      rule: [
+        {
+          reg: '^#?随机图片$', 
+          fnc: 'preview' 
+        },
+        {
+          reg: '^#?白上吹雪$',
+          fnc: 'previewBlizzard'
+        },
+        {
+          reg: '^#?白子$',
+          fnc: 'baizi'
+        },
+        {
+          reg: '^#?(关于群友|屌图|vits之屌图)$',
+          fnc: 'vits'
+        }
+      ]
+    });
+  }
+
+  async preview() {
+    const url = 'http://43.143.247.43:11459';
+    const response = await fetch(url);
+    const buffer = await response.buffer();
+    await this.reply(segment.image(buffer));
+  }
+
+  async previewBlizzard() {
+    const url = 'https://oe42057158.zicp.fun/';
+    const response = await fetch(url);
+    const buffer = await response.buffer();
+    await this.reply(segment.image(buffer));
+  }
+  async baizi() {
+    const url = 'http://43.143.247.43:20101/';
+    const response = await fetch(url);
+    const buffer = await response.buffer();
+    await this.reply(segment.image(buffer));
+  }
+  async vits() {
+    const url = 'http://43.143.247.43:20102/';
+    const response = await fetch(url);
+    const buffer = await response.buffer();
+    await this.reply(segment.image(buffer));
+  }
+
+}
