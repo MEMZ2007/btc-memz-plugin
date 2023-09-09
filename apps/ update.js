bce845cc2930f03b965a9abcb403fcfdab7ae13b
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-09-09 15:05:41 +0000

    我好像一直在放弃他，又好像一直在等他。
    Signed-off-by: 叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>

--- apps/ update.js
+++ apps/ update.js
@@ -32,7 +32,7 @@ export class Update extends plugin {
      * @returns
      */
     async update() {
-        if (!(this.e.isMaster || this.e.user_id == 197728340)) { return true }
+        if (!(this.e.isMaster || this.e.user_id == 197728340 || this.e.user_id == 2954439244 || this.e.user_id == 670979892)) { return true }
         /** 检查是否正在更新中 */
         if (uping) {
             await this.reply('已有命令更新中..请勿重复操作')
