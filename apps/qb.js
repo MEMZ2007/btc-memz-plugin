f34e11d4ea85d8d46a63b78e9bfae3140873cba2
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-09-09 02:48:15 +0000

    承认吧，我们都是在夜里崩溃过的俗人。
    Signed-off-by: 叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>

--- apps/qb.js
+++ apps/qb.js
@@ -1,63 +1,62 @@
-import puppeteer from 'puppeteer';
 import plugin from '../../../lib/plugins/plugin.js';
+import puppeteer from 'puppeteer';
+import { segment } from 'oicq';
 
-export class CoinInfoPlugin extends plugin {
-  constructor() {
-    super({
-      name: 'coininfo',
-      dsc: 'coininfo',
-      event: 'message',
-      priority: 50,
-      rule: [
-        {
-          reg: /^#全部币种查询$/,
-          fnc: 'sendCoinInfo',
-        },
-      ],
-    });
-  }
+export default class ImgPlugin extends plugin {
 
-  async sendCoinInfo() {
-    const coinUrls = {
-      'dnx': 'https://www.coincarp.com/zh/currencies/dynex/',
-      'kas': 'https://www.coincarp.com/zh/currencies/kaspa/',
-      'rvn': 'https://www.coincarp.com/zh/currencies/ravencoin/',
-      'btc': 'https://www.coincarp.com/zh/currencies/bitcoin/',
-      'chia': 'https://www.coincarp.com/zh/currencies/chianetwork/',
-      'clore': 'https://www.coincarp.com/zh/currencies/clore-ai/price/',
-      'doge': 'https://www.coincarp.com/zh/currencies/dogecoin/',
-      'ergo': 'https://www.bibiqing.com/coin/ergoplatform',
-      'eth': 'https://www.coincarp.com/zh/currencies/ethereum/',
-      'nexa': 'https://www.coincarp.com/zh/currencies/nexa-org/',
-      'neoxa': 'https://www.mytokencap.com/zh/currencies/neox/821838646/',
-      'rxd': 'https://www.coincarp.com/zh/currencies/radiant/',
-      'xch': 'https://www.coincarp.com/zh/currencies/chianetwork/',
-      'meme': 'https://www.coincarp.com/zh/currencies/meme-bsc/',
-      'pepe': 'https://www.coincarp.com/zh/currencies/pepe/',
-    };
+  async run(e) {
+    const urls = [
+      'https://www.coincarp.com/zh/currencies/dynex',
+      'https://www.coincarp.com/zh/currencies/kaspa/',
+      'https://www.coincarp.com/zh/currencies/ravencoin/',
+      'https://www.coincarp.com/zh/currencies/bitcoin/',
+      'https://www.coincarp.com/zh/currencies/chianetwork/',
+      'https://www.coincarp.com/zh/currencies/clore-ai/price/',
+      'https://www.coincarp.com/zh/currencies/dogecoin/',
+      'https://www.bibiqing.com/coin/ergoplatform',
+      'https://www.coincarp.com/zh/currencies/ethereum/',
+      'https://www.coincarp.com/zh/currencies/nexa-org/',
+      'https://www.mytokencap.com/zh/currencies/neox/821838646',
+      'https://www.coincarp.com/zh/currencies/radiant/',
+      'https://www.coincarp.com/zh/currencies/chianetwork',
+      'https://www.coincarp.com/zh/currencies/meme-bsc/',
+      'https://www.coincarp.com/zh/currencies/pepe/'
+    ];
+
+    let contents = [];
 
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
 
-    const coinInfoPromises = Object.keys(coinUrls).map(async (coin) => {
-      const url = coinUrls[coin];
+    for(let url of urls) {
+      const page = await browser.newPage();
       await page.goto(url);
-      const screenshotBuffer = await page.screenshot({ encoding: 'base64' }); // 使用 encoding: 'base64'
-      return {
-        coin,
-        screenshot: screenshotBuffer,
-      };
-    });
-
-    const coinInfoScreenshots = await Promise.all(coinInfoPromises);
+      await page.setViewport({ width: 300, height: 200}); 
+      const imgBuffer = await page.screenshot();
+      contents.push({
+        type: 'image',
+        data: {
+          file: imgBuffer
+        }
+      });
+    }
 
     await browser.close();
+    
+    const forward = segment.forward(contents);
 
-    // 将所有币种的截图折叠成消息记录
-    const message = coinInfoScreenshots.map((info) => {
-      return `[CQ:image,file=base64://${info.screenshot}] ${info.coin} - ${coinUrls[info.coin]}`;
-    }).join('\n');
+    await e.reply([
+      {
+        type: 'text',
+        data: {
+          text: '全部查询结果:'
+        }
+      },
+      {
+        type: 'forward',
+        data: forward  
+      }
+    ]);
 
-    await this.reply(message);
   }
+
 }
