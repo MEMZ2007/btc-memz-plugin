0cade6fecdeece738958d24ed5d5cfb18425c9f9
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-09-15 10:21:19 +0000

    update apps/api/cnm.js.
    Signed-off-by: 叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>

--- apps/api/cnm.js
+++ apps/api/cnm.js
@@ -4,36 +4,6 @@ import fetch from 'node-fetch';
 export class cnm extends plugin {
   constructor() {
     super({
-      name: '骚话文案',
-      dsc: '获取骚话文案',
-      event: 'message',
-      priority: 5000,
-      rule: [
-        {
-          reg: '^#?骚话文案$',
-          fnc: 'getSaohuaWenAn',
-        },
-      ],
-    });
-  }
-
-  async getSaohuaWenAn(e) {
-    try {
-      const response = await fetch('https://v.api.aa1.cn/api/api-saohua/index.php?type=json');
-      const data = await response.json();
-      await this.reply(data.saohua, true, { recallMsg: 30 });
-    } catch (error) {
-      console.error('[骚话文案] 接口请求失败:', error);
-      await this.reply('骚话文案接口请求失败，请稍后重试');
-    }
-  }
-}
-import plugin from '../../../../lib/plugins/plugin.js';
-import fetch from 'node-fetch';
-
-export class cnm extends plugin {
-  constructor() {
-    super({
       name: 'btc-cnm',
       dsc: 'btc-cnm',
       event: 'message',
