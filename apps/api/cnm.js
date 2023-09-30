ada80c80d4c13634f4007ccc244f4c2afbebc0e2
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-09-30 12:33:22 +0000

    update apps/api/cnm.js.
    Signed-off-by: 叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>

--- apps/api/cnm.js
+++ apps/api/cnm.js
@@ -18,20 +18,22 @@ export class RidiculePlugin extends plugin {
     });
   }
 
-  async btccnm(e) {
-    try {
-      const url = 'http://api.wxsszs.cn/api/Ridicule.php?msg=5';
-      
-      const response = await fetch(url);
-      const data = await response.json();
-      
-      if (response.ok) {
-        await this.reply(data.content, true, {recallMsg: 30});
-      } else {
-        throw new Error('接口请求失败');
-      }
-      
-    } catch (error) {
+  async btccnmm(e) {
+  try {
+    const url = 'http://api.wxsszs.cn/api/Ridicule.php?msg=5';
+    
+    const params = {msg: 5}; 
+    const response = await fetch(url + '?' + new URLSearchParams(params));
+    
+    const data = await response.json();
+    
+    if (response.ok) {
+      await this.reply(data.content, true, {recallMsg: 30});
+    } else {
+      throw new Error('接口请求失败');
+    }
+    
+  } catch (error) {
       console.error('cnm接口请求失败:', error);
       await this.reply('cnm接口请求失败,请稍后重试');
     }
