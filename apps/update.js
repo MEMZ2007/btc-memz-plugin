d3edbe89866fa756740f457a9d6f9092eee878f0
叶殇 <12251183+maple-leaf-sweeping@user.noreply.gitee.com>
2023-11-03 11:07:09 +0800

    优化bug

--- apps/update.js
+++ apps/update.js
@@ -23,7 +23,11 @@ export class Update extends plugin {
                 {
                     reg: '^#*(btc|BTC)(插件)?(强制)?更新$',
                     fnc: 'update'
-                }
+                },
+                {
+                    reg: '^#(BTC|btc)(插件)?更新日志',
+                    fnc: 'btcupdateLog'
+                  },
             ]
         })
     }
@@ -246,4 +250,9 @@ export class Update extends plugin {
         // 返回true，表示安装了git
         return true
     }
+    async btcupdateLog () {
+        const plugin = this.getPlugin()
+        if (plugin === false) return false
+        return this.reply(await this.getLog(plugin))
+      }
 }
