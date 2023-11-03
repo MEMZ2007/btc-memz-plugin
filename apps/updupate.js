50feef5f871660a077df4058e46b392bcd32a257
枫林 <670979892@qq.com>
2023-11-03 21:33:54 +0800

    更新bug

---
+++ apps/updupate.js
@@ -0,0 +1,30 @@
+import { exec } from 'child_process'
+import plugin from '../../../lib/plugins/plugin.js'
+import { createRequire } from 'module'
+import _ from 'lodash'
+import common from "../../../lib/common/common.js"
+import { ThePath } from "../app.config.js"
+
+export class Bup extends plugin {
+  // 构造函数，接收一个参数e
+  constructor(e) {
+    // 定义一个任务对象
+    this.task = {
+      // 任务调度，每天4点执行一次
+      cron: '0 0 4 * * ?',
+      // 任务名称
+      name: 'BTC自动更新任务',
+      // 任务函数
+      fnc: 'bupdate',
+    }
+  }
+  async bupdate() {
+    // 执行强制更新操作
+    let command = `git -C ${ThePath} pull --no-rebase`
+    if (True) {
+      command = `git -C ${ThePath} reset --hard origin && ${command}`
+      logger.info(logger.blue('BTC插件正在执行更新操作...'))
+    }
+    return true
+  }
+}
